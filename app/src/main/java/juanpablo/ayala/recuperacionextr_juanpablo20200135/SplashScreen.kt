b27aec38c1775b476e84d7331715c3bf018acfd5
Juan Pablo <20200135@ricaldone.edu.sv>
2024-11-11 09:32:39 -0600

    Splash Screen terminado

--- app/src/main/java/juanpablo/ayala/recuperacionextr_juanpablo20200135/SplashScreen.kt
+++ app/src/main/java/juanpablo/ayala/recuperacionextr_juanpablo20200135/SplashScreen.kt
@@ -1,6 +1,9 @@
 package juanpablo.ayala.recuperacionextr_juanpablo20200135
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,9 @@ class SplashScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, Registro::class.java))
+            finish()
+        },3000)
     }
 }
